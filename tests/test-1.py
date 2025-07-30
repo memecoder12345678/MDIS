087299fae6d9d8ac2fa985f369d9863ade390e46
memecoder <memecoder17@gmail.com>
2025-07-30 17:16:47 +0700

    Update test-1.py

--- tests/test-1.py
+++ tests/test-1.py
@@ -1,7 +1,7 @@
 from MDIS import MDISParser
 
 # Test case
-test_cases = ["MSE:McAfee.I.A#Bypass_FakeAV_Joker_Adware!Phish ", "MSE:Avast.I.A#Bypass_FakeAV_Joker_Adware_Stealer_Resurrector_Spy!Phish"]
+test_cases = ["MSE:McAfee.I.A#Bypass_FakeAV_Joker_Adware!Phish_PUA ", "MSE:Avast.I.A#Bypass_FakeAV_Joker_Adware_Stealer_Resurrector_Spy!Phish_PUA"]
 for id in test_cases:
     print(
         MDISParser(id).to_natural()
